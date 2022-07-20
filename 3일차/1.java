a9c819a9aa8437b97d212a75aea6f2615a891a4c
wjh2335 <38124814+wjh2335@users.noreply.github.com>
2022-07-20 16:28:27 +0900

    Create 1.java

---
+++ 3일차/1.java
@@ -0,0 +1,39 @@
+package Practice;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Practice
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		Stack<Integer> stack = new Stack<Integer>();
+		StringBuffer ans = new StringBuffer();
+		
+		int n = input.nextInt(), cnt = 0;
+		boolean flag = true;
+		
+		for(int i = 0; i < n; ++i)
+		{
+			int a = input.nextInt();
+			
+			while(cnt < a)
+			{
+				stack.push(++cnt);
+				ans.append("+\n");
+			}
+			if(stack.peek() == a)
+			{
+				stack.pop();
+				ans.append("-\n");
+			}
+			else flag = false;
+			
+		}
+		
+		if(flag) System.out.println(ans);
+		else System.out.println("NO");
+		
+		input.close();
+	}
+}
