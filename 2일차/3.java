dbc0d3991c77a2e03afe4ed78922911b08958b34
wjh2335 <38124814+wjh2335@users.noreply.github.com>
2022-07-20 14:20:26 +0900

    Create 3.java

---
+++ 2일차/3.java
@@ -0,0 +1,33 @@
+package pack;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class main
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		int n = input.nextInt(), m = input.nextInt();
+		int arr[] = new int[n];
+		
+		for(int i = 0; i < n; ++i) arr[i] = input.nextInt();
+		Arrays.sort(arr);
+		
+		int i = 0, j = n - 1, ans = 0;
+		while(i < j)
+		{
+			if(arr[i] + arr[j] < m) ++i;
+			else if(arr[i] + arr[j] > m) --j;
+			else
+			{
+				++ans;
+				++i; --j;
+			}
+		}
+		
+		System.out.println(ans);
+		
+		input.close();
+	}
+}
