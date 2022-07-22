fdfe2b32d3c56233533ee3ccbd600ce4f3e26166
wjh2335 <38124814+wjh2335@users.noreply.github.com>
2022-07-22 14:04:26 +0900

    Create 3.java

---
+++ 3일차/3.java
@@ -0,0 +1,25 @@
+package pack;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class main
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		Queue<Integer> que = new LinkedList<>();
+		
+		int n = input.nextInt();
+		for(int i = 1; i <= n; ++i) que.add(i);
+		
+		while(que.size() != 1)
+		{
+			que.poll();
+			que.add(que.poll());
+		}
+    
+		System.out.println(que.poll());
+		input.close();
+	}
+}
