d95f69d2ca2c935a51298a4f2f4c9601e7c705be
wjh2335 <38124814+wjh2335@users.noreply.github.com>
2022-07-27 11:54:11 +0900

    Create 2.java

---
+++ 5일차/2.java
@@ -0,0 +1,48 @@
+package pack1;
+import java.util.Scanner;
+
+public class main
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+						
+		int n = input.nextInt(), arr[] = new int[n], max = 0;
+		
+		int[][] que = new int[10][1000];
+		int[] head = new int[10], tail = new int[10];
+		
+		for(int i = 0; i < n; ++i)
+		{
+			arr[i] = input.nextInt();
+			if(max < arr[i]) max = arr[i];
+		}
+		int maxDigit = -1;
+
+		while(max > 0)
+		{
+			++maxDigit;
+			max /= 10;
+		}
+
+		int t1 = 10, t2 = 1;
+		for(int k = 0; k < maxDigit; ++k)
+		{
+			for(int i = 0; i < n; ++i) que[arr[i] % t1 / t2][tail[arr[i] % t1 / t2]++] = arr[i];
+			int j = 0;
+			for(int i = 0; i < 10; ++i)
+			{
+				while(head[i] != tail[i]) arr[j++] = que[i][head[i]++];
+			}
+		
+			for(int i = 0; i < j; ++i) System.out.println(arr[i]);
+			t1 *= 10; t2 *= 10;
+		}
+		input.close();
+	}
+}
+/*
+% 10 / 1
+% 100 / 10
+% 1000 / 100
+*/
